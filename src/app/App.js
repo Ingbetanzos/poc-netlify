ae9f25943d4db238c47894c7f41f22fe274dba24
Sergio Shovanny Hernández Betanzos <shovanny@Sergios-MacBook-Air.local>
2025-05-04 19:34:18 -0600

    feat: Initialize project

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+    const getTitle = () => {
+        return "App betz poc"
+    }
+    return (
+        <h1>{getTitle()}</h1>
+    );
+}
+
+export default App;
